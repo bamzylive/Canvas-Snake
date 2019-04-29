4c28ab95c73302bef574d8ac0f414d4ddfbf5d1f
bamzylive <bamzy@live.com>
2019-04-29 16:15:00 +0800

    great improvement

--- play.js
+++ play.js
@@ -1,12 +1,21 @@
-var c = document.getElementById("myCanvas");
-var cxt = c.getContext("2d");
-var first = new Array(0, 0);
-var grid = new Array(30), move = new Array(1, 0);
-for (var i = 0; i < 30; i++) {
+let c = document.getElementById("myCanvas");
+let cxt = c.getContext("2d");
+let head = {x: 0, y: 0};	// Starts at the upperleft conner,
+let move = {dx: 1, dy: 0};	// while heading right.
+let grid = new Array(30);	// info of each block of the field
+
+// initialize
+for (let i = 0; i < 30; i++) {
 	grid[i] = new Array(30);
-	for (var j = 0; j < 30; j++)
+	for (let j = 0; j < 30; j++)
 		grid[i][j] = 0;
 }
+grid[0][0] = 1;
+grid[15][15] = -1;
+
+// main
+document.onkeyup = command;
+render();
 
 function drawCircle(a, b) {
 	cxt.fillStyle = "white";
@@ -17,53 +26,53 @@ function drawCircle(a, b) {
 }
 
 function addPoint() {
-	var a = 1, b = 1;
+	let a = 1, b = 1;
 	a = Math.floor(Math.random() * 30);
 	b = Math.floor(Math.random() * 30);
 	grid[a][b] = -1;
 }
 
-function draw() {
-	document.onkeyup = command;
-	if (grid[ first[0]+move[0] ][ first[1]+move[1] ] < 0) {
+function render() {
+	let newHead = { x: head.x + move.dx, y: head.y + move.dy };
+
+	if (grid[ newHead.x ][ newHead.y ] < 0) {
+		// food caught, make it the new head and generate new food
+		grid[ newHead.x ][ newHead.y ] = grid[ head.x ][ head.y ] + 1;
+		head = newHead;
 		addPoint();
-		first[0] += move[0];
-		first[1] += move[1];
-		grid[ first[0] ][ first[1] ] = grid[ first[0]-move[0] ][ first[1]-move[1] ] + 1;
 	} else {
-		first[0] += move[0];
-		first[1] += move[1];
-		grid[ first[0] ][ first[1] ] = grid[ first[0]-move[0] ][ first[1]-move[1] ] + 1;
-		for (var i = 0 ; i < 30; i++)
-			for (var j = 0; j < 30; j++)
+		// snake creeps
+		if (newHead.x < 0 || newHead.x >=  30 || 
+			newHead.y < 0 || newHead.y >= 30) {
+			// game over
+			alert("You lose.");
+			return;
+		}
+		grid[ newHead.x ][ newHead.y ] = grid[ head.x ][ head.y ] + 1;
+		head = newHead;
+		for (let i = 0 ; i < 30; i++)
+			for (let j = 0; j < 30; j++)
 				if (grid[i][j] > 0)
 					grid[i][j]--;
 	}
-	for (var i = 0; i < 30; i++)
-		for (var j = 0; j < 30; j++)
+
+	// the render process
+	cxt.fillStyle = "black";
+	cxt.fillRect(0, 0, 600, 600);
+	for (let i = 0; i < 30; i++)
+		for (let j = 0; j < 30; j++)
 			if (grid[i][j] != 0)
 				drawCircle(i * 20 + 10, j * 20 + 10);
-	var t = setTimeout("clear()", 200);
+
+	let timerId = setTimeout(render, 175);
 }
 
 function command(event) {
-	var key = event.keyCode;
+	let key = event.keyCode;
 	switch(key) {
-		case 37: move[0] = -1; move[1] = 0; break;
-		case 38: move[0] = 0; move[1] = -1; break;
-		case 39: move[0] = 1; move[1] = 0; break;
-		case 40: move[0] = 0; move[1] = 1; break;
+		case 37: move.dx = -1; move.dy = 0; break;
+		case 38: move.dx = 0; move.dy = -1; break;
+		case 39: move.dx = 1; move.dy = 0; break;
+		case 40: move.dx = 0; move.dy = 1; break;
 	}
 }
-
-function clear() {
-	cxt.fillStyle = "black";
-	cxt.fillRect(0, 0, 600, 600);
-	draw();
-}
-
-grid[0][0] = 1;
-grid[15][15] = -1;
-move[0] = 1;
-move[1] = 0;
-draw();
